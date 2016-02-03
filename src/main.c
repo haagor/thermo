de13f147c9647dac827659c3f5bfd06fb66d21c7
Simon PARIS <ps205947@etu.unice.fr>
2016-02-03 10:20:16 +0100

    init matrice

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
-/* Hello World program */
+#include <stdio.h>
+#include <stdlib.h>
 
-#include<stdio.h>
-
+int
 main()
 {
-    printf("Hello World\n");
+	int n = 4;
+	int matrice[n][n]; 
+
+	int row, column;
+	for (row = 0; row < n; row++)
+	{
+		printf("\n");
+		for (column = 0; column < n; column++)
+		{
+			matrice[row][column] = 0;
+			printf("%d	", matrice[row][column]);
+		}
+	}
+	printf("\n");
 }
