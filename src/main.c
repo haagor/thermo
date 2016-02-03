822ab5bd5c3cca7fb959ea693c32ab5e8c2a4037
Simon PARIS <ps205947@etu.unice.fr>
2016-02-03 21:37:28 +0100

    makefile

--- src/main.c
+++ src/main.c
@@ -1,19 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int
 main()
 {
-	int n = 4;
-	int matrice[n][n]; 
+
+	int T = 36;
+	float n = 4;						// [4-9]
+	int N = pow(2,n);					// taille de la matrice
+	int matrice[N][N];					// init matrice
+
+	// calcul des cellules chauffées
+	float heat = pow(2,(n - 1));		
+	int miHeat = heat - pow(2,(n-4));
+	int maHeat = heat + pow(2,(n-4));
 
 	int row, column;
-	for (row = 0; row < n; row++)
+	int red = 0;
+	for (row = 0; row < N; row++)
 	{
-		printf("\n");
-		for (column = 0; column < n; column++)
+		if (row >= miHeat && row <= maHeat)
 		{
-			matrice[row][column] = 0;
+			red = 1;
+		} else {
+			red = 0;
+		}
+		printf("\n");
+		for (column = 0; column < N; column++)
+		{
+			if (column >= miHeat && column <= maHeat && red)
+			{
+				matrice[row][column] = T;
+			} else {
+				matrice[row][column] = 0;
+			}
 			printf("%d	", matrice[row][column]);
 		}
 	}
