3a96e6a1fec9114219db208400de04e84dab8802
Simon PARIS <ps205947@etu.unice.fr>
2016-04-23 18:53:23 +0200

    e2

---
+++ src/e2.orig/heaTransfert.h
@@ -0,0 +1,9 @@
+/*
+* Author : Simon Paris
+* 2016-03-08
+*
+* fonctions relatives aux itérations de transfert de chaleur
+*/
+
+// instancie, initialise la matrice et lance les iterations
+void e2_run(int size, float T, int nb_iter, int nb_thread, int print);
